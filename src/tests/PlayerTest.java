f339ef5427a2f9e5967749869d9f519f81ada80d
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2015-04-11 15:18:11 +0100

    Player created

---
+++ src/tests/PlayerTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**Tests for the Player object. Apart from getters Test, ensures that a Player cannot have  a null name
+ * Also tests that it is immutable.
+ * 
+ * @author Jamie
+ *
+ */
+public class PlayerTest {
+
+	@Test
+	public void  {
+		
+	}
+
+}
