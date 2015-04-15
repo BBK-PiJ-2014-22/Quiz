72a95c255117be9180e9bcac872fa5d1347d6989
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2015-04-15 14:40:47 +0100

    Game basic object created
    getters
    constructor (without functionality)
    methods created(without functionality)
    hash code, toString and Equals generated

---
+++ src/tests/GameTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetNextQuestion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAnswerQuestion() {
+		fail("Not yet implemented");
+	}
+
+}
