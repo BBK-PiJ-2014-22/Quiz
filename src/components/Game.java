ef267e1c92de2ef3cb6db0459f87d8945445f268
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2015-04-15 14:49:07 +0100

    Game basic object created
    see previous commit. File not flagged.

---
+++ src/components/Game.java
@@ -0,0 +1,115 @@
+package components;
+
+import java.util.List;
+import components.QuizStatus;
+
+/**The Game object represents in in progress game of a specific quiz. 
+ * When it is not completed, the player will be able to answer the next question.
+ * Once the last question is answered, it will be set to completed,  a score will be 
+ * calculated and it will be closed. Games can only be created for active quizzes - 
+ * inactive or completed quizzes cannot have games. If a game is set to completed mid quiz,
+ * the game will be completed and the score set to 0. 
+ * 
+ * @author Jamie MacIver
+ *
+ */
+public class Game {
+	
+	//Note: no normal setters for any fields
+	private Player player;  		//Set only by constructor
+	private Quiz quiz; 				//Set only by constructor
+	private List<Integer> answers;	//Added to by answerQuestion()
+	int score;						//Set after answering final question by completeGame method
+	boolean completed;				//Set after answering final question by completeGame method
+	
+	//Constructors
+	
+	/**Does not work if quiz is inactive or completed
+	 * 
+	 * @param player
+	 * @param quiz
+	 */
+	public Game(Player player, Quiz quiz){
+		
+	}
+	
+	
+	//Standard methods
+	@Override
+	public String toString() {
+		return "Game [player=" + player + ", quiz=" + quiz + ", answers="
+				+ answers + ", score=" + score + ", completed=" + completed
+				+ "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((answers == null) ? 0 : answers.hashCode());
+		result = prime * result + (completed ? 1231 : 1237);
+		result = prime * result + ((player == null) ? 0 : player.hashCode());
+		result = prime * result + ((quiz == null) ? 0 : quiz.hashCode());
+		result = prime * result + score;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		if (answers == null) {
+			if (other.answers != null)
+				return false;
+		} else if (!answers.equals(other.answers))
+			return false;
+		if (completed != other.completed)
+			return false;
+		if (player == null) {
+			if (other.player != null)
+				return false;
+		} else if (!player.equals(other.player))
+			return false;
+		if (quiz == null) {
+			if (other.quiz != null)
+				return false;
+		} else if (!quiz.equals(other.quiz))
+			return false;
+		if (score != other.score)
+			return false;
+		return true;
+	}
+	
+	//Getters
+	public Player getPlayer() {
+		return player;
+	}
+	public Quiz getQuiz() {
+		return quiz;
+	}
+	public List<Integer> getAnswers() {
+		return answers;
+	}
+	public int getScore() {
+		return score;
+	}
+	public boolean isCompleted() {
+		return completed;
+	}
+
+	//Game methods
+	public String getNextQuestion(){
+		return null;
+	}
+	
+	public boolean answerQuestion(){
+		return false;
+	}
+	
+	private void completeGame(){
+		
+	}
+}
--- src/tests/GameTest.java
+++ src/tests/GameTest.java
@@ -1,14 +1,28 @@
 package tests;
 
+
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 
+import components.Player;
+import components.Quiz;
+import components.QuizStatus;
+import components.Game;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) 
 public class GameTest {
 
+	Player player;
+	Quiz quiz;
+	Game game;
+	
 	@Before
 	public void setUp() throws Exception {
+		
 	}
 
 	@Test
