bb8c7b3e8beab0c6b2898e138e68e7bb1ea40c73
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2015-04-25 18:42:44 +0100

    Log added to provide standard output to server side

---
+++ src/network/Log.java
@@ -0,0 +1,15 @@
+package network;
+
+/**Used to log any activities. Currently set to just print but may be altered
+ * 
+ * 
+ * @author Jamie
+ *
+ */
+public class Log {
+	
+	public static void log(String message){
+		System.out.println(message);
+	}
+
+}
