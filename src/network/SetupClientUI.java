f9ffbf13f18ce01a843a3e6a5ce62f03c8d36b96
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2015-04-26 21:55:41 +0100

    setupClient complete

--- src/network/SetupClientUI.java
+++ src/network/SetupClientUI.java
@@ -1,12 +1,22 @@
 package network;
 
 import java.rmi.RemoteException;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 import components.Player;
+import components.Question;
 
+
+/**The SetupClientUI class exists exclusively to handle the User Interface for the Setup Client
+ * All methods contained handle input from the console and printing of information back to the 
+ * console, with a few exceptions (non-void return types) which pass Strings for use in various
+ * places throughout.
+ * 
+ * 
+ * @author Jamie
+ *
+ */
 public class SetupClientUI {
 	
 	SetupClient client;
@@ -17,6 +27,9 @@ public class SetupClientUI {
 		scui.launch();
 	}
 
+	/**The launch method loads a scanner, and then runs methods to display
+	 * welcome message, options selection and exit.
+	 */
 	public void launch(){
 		this.client = new SetupClient();
 		client.launch();
@@ -34,6 +47,9 @@ public class SetupClientUI {
 	}
 	
 		
+	/**Prints out a welcome message
+	 * 
+	 */
 	private void welcome(){
 		System.out.println("\n\nWelcome to the QuizServer Setup Client.");
 		System.out.println("This client will allow you to create quizzes for people "
@@ -85,7 +101,8 @@ public class SetupClientUI {
 		}
 	}
 	
-	/**Manages options selection and will run appropriate 
+	/**Manages options selection for the top layer and will run appropriate methods depending on option selected.
+	 * Considered the main menu.
 	 * 
 	 * @throws RemoteException
 	 */
@@ -119,11 +136,13 @@ public class SetupClientUI {
 							break;
 					case 6: this.viewGames();
 							break;
-					case 7: this.getWinners();
+					case 7: this.printWinners();
 							break;
 					case 8: this.getHelp();
 							break;
 					case 0: running = false;
+							break;
+					default: System.out.println("Invalid option");
 							break;
 				}
 				
@@ -134,7 +153,8 @@ public class SetupClientUI {
 	}
 	
 	
-	/**Handles the UI for a user to create a new quiz. They will be able to edit the quiz afterwards.
+	/**Handles the UI for a user to create a new quiz. They will be able to edit the quiz afterwards
+	 * if they make the correct selection
 	 * 
 	 * @throws RemoteException
 	 */
@@ -151,7 +171,6 @@ public class SetupClientUI {
 			client.editQuiz(result);
 			editQuiz();
 		}
-		debug(proceed);
 	}
 	
 	/**User Interface for editing a quiz. First a quiz must be loaded into the client's
@@ -179,22 +198,26 @@ public class SetupClientUI {
 		
 		if (client.currentQuiz != null){
 			System.out.println("What would you like to edit?");
+			editOptions();
 			
 		}
 
 		client.currentQuiz = null; //Should be a method
 	}
 	
+	/** Manages UI for editing a quiz. Considered the Quiz Edit menu.
+	 * 
+	 * @throws RemoteException
+	 */
 	private void editOptions() throws RemoteException{
 		boolean running = true;
 		while (running) {
+			System.out.println("\nEditing Quiz:");
+			printQuiz();
 			System.out.println("\nPlease select an option from the list:");
 			System.out.println("		[1] - Add a Question");
 			System.out.println("		[2] - Edit a Question");
-			System.out.println("		[3] - Swap question order");
-			System.out.println("		[4] - Change quiz name");
-			System.out.println("		[5] - View all Questions");
-			System.out.println("		[6] - Help");
+			System.out.println("		[3] - Help");
 			System.out.println("		[0] - Exit to Menu");
 			
 			try{
@@ -205,19 +228,11 @@ public class SetupClientUI {
 							break;
 					case 2: this.editQuestion();
 							break;
-					case 3: this.swapQuestion();
-							break;
-					case 4: this.changeQuizName();
-							break;
-					case 5: this.viewQuestions();
-							break;
-					case 6: this.editQuizHelp();
-							break;
-					case 7: this.getWinners();
-							break;
-					case 8: this.getHelp();
+					case 3: this.editQuizHelp();
 							break;
 					case 0: running = false;
+							break;
+					default: System.out.println("Invalid option");
 							break;
 				}
 				
@@ -227,8 +242,179 @@ public class SetupClientUI {
 		}
 	}
 	
+	/**Allows the user to add a question to the current quiz
+	 * 
+	 * @throws RemoteException
+	 */
+	private void addQuestion() throws RemoteException {
+		System.out.println("Enter question");
+		client.addQuestion(sc.nextLine());
+		System.out.println("Would you like to edit this question? (y to edit)");
+		if (sc.nextLine().toLowerCase().equals("y")) editQuestion();
+	}
+	
+	/**Loads a question to the client.currentQuestion to allow editing.
+	 * 
+	 * @throws RemoteException
+	 */
+	private void editQuestion() throws RemoteException {
+		System.out.println(client.currentQuestion.display());
+		//Will run if current question is null
+		//If this has been loaded from addQuestion the question will already be assigned
+		while (client.currentQuestion == null){
+			System.out.println("Please enter question id");
+			System.out.println("Enter -1 to return to menu");
+			try{
+				int id = Integer.parseInt(sc.nextLine());
+				if (id == -1) break;
+				else if (client.editQuestion(id)) System.out.println("Now editing question "+id);
+			}catch (NumberFormatException ex){
+				System.out.println("Invalid entry. Please enter an ID");
+			}
+		}
+		questionOptions();
+		client.currentQuestion = null;
+	}
+	
+	/**Displays help for the edit page
+	 * 
+	 */
+	private void editQuizHelp() {
+		// TODO Auto-generated method stub
 		
-	/**
+	}
+	
+	/**Options page for editing questions within a quiz
+	 * 
+	 * @throws RemoteException
+	 */
+	private void questionOptions() throws RemoteException {
+		boolean running = true;
+		while (running) {
+			System.out.println("Editing Question:");
+			printQuestion();
+			System.out.println("\nPlease select an option from the list:");
+			System.out.println("		[1] - Add an answer");
+			System.out.println("		[2] - Swap two answers");
+			System.out.println("		[3] - Set correct answer");
+			System.out.println("	    [4] - Change an answer");
+			System.out.println("		[0] - Exit to Menu");
+			
+			try{
+				int option = Integer.parseInt(sc.nextLine());
+
+				switch  (option){
+					case 1: this.addAnswer();
+							break;
+					case 2: this.swapAnswer();
+							break;
+					case 3: this.setCorrectAnswer();
+							break;
+					case 4: this.changeAnswer();
+							break;
+					case 0: running = false;
+							break;
+					default: System.out.println("Invalid option");
+							break;
+				}
+				
+			}catch (NumberFormatException ex){
+				System.out.println("Not a valid option");
+			}
+		}
+		
+	}
+	
+	/**Allows the user to change an answer's wording
+	 * 
+	 * @throws RemoteException
+	 */
+	private void changeAnswer() throws RemoteException {
+		System.out.println("\nEnter ID followed by new answer (e.g. 0 New Answer)");
+		
+		boolean changed = false;
+		while (!changed){
+			String line = sc.nextLine();
+			int firstSpace = line.indexOf(' ');
+			int id;
+			try{
+				id = Integer.parseInt(line.substring(0, firstSpace));
+				String answer = line.substring(firstSpace,line.length());
+				changed = client.changeAnswer(id, answer);
+				if (changed) System.out.println("Answer changed\n");
+				else System.out.println("Unable to change answer. Check entry\n");				
+			}catch (NumberFormatException ex){
+				System.out.println("Please put ID first\n");
+			}
+		}
+	}
+
+	/**Allows the user to change the correct answer
+	 * 
+	 * @throws RemoteException
+	 */
+	private void setCorrectAnswer() throws RemoteException {
+		boolean set = false;
+		while (!set){
+			System.out.println("Please enter answer id");
+			System.out.println("Enter -1 to return to menu");
+			try{
+				int id = Integer.parseInt(sc.nextLine());
+				if (id == -1) break;
+				else if (client.setCorrectAnswer(id)) set = true;
+			}catch (NumberFormatException ex){
+				System.out.println("Invalid entry. Please enter an ID");
+			}
+			System.out.println("Correct answer set");
+		}
+		
+	}
+
+	/**Allows the user to add a new answer
+	 * 
+	 * @throws RemoteException
+	 */
+	private void addAnswer() throws RemoteException {
+		boolean added = false;
+		while (!added){
+			System.out.println("\nEnter new answer");
+			String answer = sc.nextLine();
+			if (answer != null) System.out.println("Answer must not be blank");
+			else client.addAnswer(answer);
+		}
+
+		
+	}
+
+	/**Allows the user to swap two answers in order
+	 * 
+	 * @throws RemoteException
+	 */
+	private void swapAnswer() throws RemoteException {
+		boolean swapped = false;
+		while (!swapped){
+			System.out.println("Enter two question IDs to swap.  -1 to exit");
+			String entry = sc.nextLine();
+			int firstSpace = entry.indexOf(' ');
+			if (entry.equals("-1")) break;
+			else if (firstSpace < 0) System.out.println("Enter two IDs separated by a space");
+			else{
+				try{
+					int id1 = Integer.parseInt(entry.substring(0, firstSpace));
+					int id2 = Integer.parseInt(entry.substring(firstSpace, entry.length()));
+					swapped = client.swapAnswer(id1, id2);
+					if (swapped) System.out.println("Questions "+id1+" and "+id2+" swapped");
+					else System.out.println("Swap unsuccessful. Check IDs are in range");
+				}catch (NumberFormatException ex){
+					System.out.println("Enter two IDs separated by a space");
+				}
+			}
+		} 			
+	}
+
+
+
+	/**Activates an inactive quiz
 	 * 
 	 * @throws RemoteException
 	 */
@@ -251,7 +437,8 @@ public class SetupClientUI {
 		}
 	}
 	
-	/**
+	/**Completes an active quiz
+	 * 
 	 * @throws RemoteException 
 	 * 
 	 */
@@ -286,25 +473,39 @@ public class SetupClientUI {
 		if (sc.nextLine().toLowerCase().equals("y")) viewQuizzes();
 	}
 	
-	/**
+	/**Displays quiz
 	 * 
 	 */
 	public void viewQuizzes() throws RemoteException{
 		System.out.println("\nAll quizzes:");
 		System.out.println(client.getPrettyQuizList());
 	}
-	/**
+	/**Prints out a list of all games for a specific quiz
 	 * 
+	 * @throws RemoteException 
 	 */
-	public void viewGames(){
-		System.out.println("Method Not Yet Implemented (viewGames)");
-		//TODO - Implement
+	public void viewGames() throws RemoteException{
+		boolean printed = false;
+		while (!printed){
+			System.out.println("Please enter quiz id");
+			System.out.println("Enter -1 to return to menu");
+			try{
+				int id = Integer.parseInt(sc.nextLine());
+				if (id == -1) break;
+				System.out.println(client.getPrettyGamesList(id));
+				printed = true;
+		}catch (NumberFormatException ex){
+				System.out.println("Invalid entry. Please enter an ID");
+			}	
+		}
 	}
-	/**
+	
+	/**Prints a list of winners for an owned quiz.
+	 * 
 	 * @throws RemoteException 
 	 * 
 	 */
-	public void getWinners() throws RemoteException{
+	public void printWinners() throws RemoteException{
 		String winners = null;
 		while (winners == null){
 			System.out.println("\nPlease enter quiz ID of a quiz you own");
@@ -320,26 +521,59 @@ public class SetupClientUI {
 		}
 	}
 	
-	/**
+	/**Prints a pretty display of the current quiz
+	 * 
+	 * @throws RemoteException
+	 */
+	public void printQuiz() throws RemoteException{
+		if (client.currentQuiz == null) System.out.println("Error: No current quiz");
+		else{
+			String result= "";
+			result += "Quiz:"+client.currentQuiz.getQuizID()+":"+client.currentQuiz.getQuizName()+"\n";
+			if (client.currentQuiz.getQuestionList() == null ||
+				client.currentQuiz.getQuestionList().size() == 0) result+= "No questions";
+			else{
+				for (Question question: client.currentQuiz.getQuestionList())
+					result += question.getQuestion()+"\n";
+			}
+			System.out.println(result);
+		}
+	}
+	
+
+	/**Prints a pretty display of the current question
+	 * 
+	 * @throws RemoteException
+	 */
+	public void printQuestion() throws RemoteException{
+		if (client.currentQuestion == null) System.out.println("Error: No current question");
+		else{
+			List<String> answerlist = client.currentQuestion.getAnswers();
+			String result= "";
+			result += "Question:"+client.currentQuestion.getQuestion()+"\n";
+			if (answerlist == null || answerlist.size() ==  0) result+= "No answers";
+			else{
+				for (String answer: answerlist)
+					result += answer+"\n";
+			}
+			System.out.println(result);
+		}
+	}
+	
+	/**Displays help for the main section
 	 * 
 	 */
 	public void getHelp(){
 		System.out.println("Method Not Yet Implemented (getHelp");
+		//TODO - Write help
 	}
 	
-	/**
+	/**Displays exit message
 	 * 
 	 */
 	public void exit(){
 		sc.close();
 		System.out.println("Thank you for using SetupClient.");
-	}
-	
-	/**
-	 * 
-	 */
-	private void debug(String string){
-		System.out.println(string);
 	}
 	
 	/**Returns a readable representation of the winners list for 
